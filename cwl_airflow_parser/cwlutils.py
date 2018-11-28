70cb5dcf64537c5e29f8b705eb47437f0c9ce050
Michael Kotliar <misha.kotliar@gmail.com>
2018-11-28 13:54:19 -0500

    Add try catch to post_state_info
    If either http_status or http_status_endpoint
    doesn't exist don't POST status updates

--- cwl_airflow_parser/cwlutils.py
+++ cwl_airflow_parser/cwlutils.py
@@ -25,9 +25,6 @@
  ****************************************************************************"""
 
 
-import os
-import sys
-import json
 import requests
 from cwltool.context import LoadingContext
 from airflow.configuration import conf
@@ -89,25 +86,32 @@ def load_tool(argsworkflow,  # type: Union[Text, Dict[Text, Any]]
 
 
 def post_state_info(context):
-    dag_run = context["dag_run"]
-    data_format = "%Y-%m-%d %H:%M:%S"
-    data = {"dag_id":         dag_run.dag_id,
-            "run_id":         dag_run.run_id,
-            "execution_date": dag_run.execution_date.strftime(data_format) if dag_run.execution_date else None,
-            "start_date":     dag_run.start_date.strftime(data_format) if dag_run.start_date else None,
-            "end_date":       dag_run.end_date.strftime(data_format) if dag_run.end_date else None,
-            "state":          dag_run.state,
-            "tasks":          []}
-    for ti in dag_run.get_task_instances():
-        data["tasks"].append({"task_id": ti.task_id,
-                              "start_date": ti.start_date.strftime(data_format) if ti.start_date else None,
-                              "end_date": ti.end_date.strftime(data_format) if ti.end_date else None,
-                              "state": ti.state,
-                              "try_number": ti.try_number,
-                              "max_tries": ti.max_tries})
+    try:
+        # Checking connection
+        http_hook = HttpHook(http_conn_id="http_status")
+        session = http_hook.get_conn()
+        url = http_hook.base_url.rstrip("/") + '/' + Variable.get("http_status_endpoint").lstrip("/")
 
-    http_hook = HttpHook(http_conn_id="http_status_update")
-    session = http_hook.get_conn()
-    url = http_hook.base_url.rstrip("/") + '/' + Variable.get("status_update_endpoint").lstrip("/")
-    prepped_request = session.prepare_request(requests.Request("POST", url, json=data))
-    http_hook.run_and_check(session, prepped_request, {})
+        # Preparing data
+        dag_run = context["dag_run"]
+        data_format = "%Y-%m-%d %H:%M:%S"
+        data = {"dag_id": dag_run.dag_id,
+                "run_id": dag_run.run_id,
+                "execution_date": dag_run.execution_date.strftime(data_format) if dag_run.execution_date else None,
+                "start_date": dag_run.start_date.strftime(data_format) if dag_run.start_date else None,
+                "end_date": dag_run.end_date.strftime(data_format) if dag_run.end_date else None,
+                "state": dag_run.state,
+                "tasks": []}
+        for ti in dag_run.get_task_instances():
+            data["tasks"].append({"task_id": ti.task_id,
+                                  "start_date": ti.start_date.strftime(data_format) if ti.start_date else None,
+                                  "end_date": ti.end_date.strftime(data_format) if ti.end_date else None,
+                                  "state": ti.state,
+                                  "try_number": ti.try_number,
+                                  "max_tries": ti.max_tries})
+
+        # Posting results
+        prepped_request = session.prepare_request(requests.Request("POST", url, json=data))
+        http_hook.run_and_check(session, prepped_request, {})
+    except Exception as e:
+        print("Failed to POST status updates:\n", e)
