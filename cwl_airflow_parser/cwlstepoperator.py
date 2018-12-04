0f43a72aaaaedccf9c3f39479fd5ce9ca1c877b8
Michael Kotliar <misha.kotliar@gmail.com>
2018-12-04 12:06:53 -0500

    Delete temp output dir when on_kill
    If CWLStepOperator is stopped, on_kill
    function tryes to delete temporary output
    directory. Additionally, if step was a
    subworkflow, several cidfiles will be read
    and several docker kill commands will be
    called

--- cwl_airflow_parser/cwlstepoperator.py
+++ cwl_airflow_parser/cwlstepoperator.py
@@ -31,6 +31,7 @@ import os, sys, tempfile
 import copy
 import glob
 import subprocess
+import shutil
 from jsonmerge import merge
 
 import schema_salad.schema
@@ -238,14 +239,23 @@ class CWLStepOperator(BaseOperator):
 
 
     def on_kill(self):
-        _logger.info('Stopping docker container')
+        _logger.info("Stop docker containers")
+        for cidfile in glob.glob(os.path.join(self.outdir, self.task_id + "*.cid")):
+            try:
+                with open(cidfile, "r") as inp_stream:
+                    _logger.debug(f"""Read container id from {cidfile}""")
+                    command = ["docker", "kill", inp_stream.read()]
+                    _logger.debug(f"""Call {" ".join(command)}""")
+                    p = subprocess.Popen(command, shell=False)
+                    try:
+                        p.wait(timeout=10)
+                    except subprocess.TimeoutExpired:
+                        p.kill()
+            except Exception as ex:
+                _logger.error(f"""Failed to stop docker container with ID from {cidfile}\n {ex}""")
+
+        _logger.info(f"""Delete temporary output directory {self.outdir}""")
         try:
-            cidfile = glob.glob(os.path.join(self.outdir, self.task_id + "*.cid"))
-            with open(cidfile[0], "r") as inp_stream:
-                p = subprocess.Popen(["docker", "kill", inp_stream.read()], shell=False)
-                try:
-                    p.wait(timeout=10)
-                except subprocess.TimeoutExpired:
-                    p.kill()
-        except Exception:
-            _logger.info('Failed to stop docker container', cidfile)
+            shutil.rmtree(self.outdir)
+        except Exception as ex:
+            _logger.error(f"""Failed to delete temporary output directory {self.outdir}\n {ex}""")
