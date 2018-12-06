e1017656bbb2d6e245df788b8c0b3de0269a4caf
Michael Kotliar <misha.kotliar@gmail.com>
2018-12-06 14:32:08 -0500

    Add jwt support for API backend
    If airflow.cfg includes
    auth_backend = cwl_airflow_parser.utils.jwt_backend
    all input requests will be checked by jwt with the
    rsa_public_key from Variables. Request data should
    have additional "check_payload" field where the
    original object is encrypted with some private key.
    "run_id" and "conf" are still present and not
    encrypted (airflow limitation). If decryption failed
    or "run_id"/"conf" doesn't correspond to their
    decrypted version, respond with Fail.
    
    Check ./utils/trigger.py to test API

---
+++ cwl_airflow_parser/utils/jwt_backend.py
@@ -0,0 +1,24 @@
+import jwt
+from functools import wraps
+from flask import request, Response
+from airflow.models import Variable
+
+
+client_auth = None
+
+
+def init_app(app):
+    pass
+
+
+def requires_authentication(function):
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        try:
+            json_data = {k: v for k, v in request.get_json(force=True).copy().items() if k != "check_payload"}
+            check_payload = jwt.decode(request.get_json(force=True)["check_payload"], Variable.get("rsa_public_key"), algorithms='RS256')
+            assert (json_data == check_payload)
+        except Exception:
+            return Response("Failed to encrypt of data is corrupted", 403)
+        return function(*args, **kwargs)
+    return decorated
